8631d123d6fd9d7119900cbc4fedfb702ebad816
Brian Allen <bdallen@mit.edu>
2018-08-28 15:48:49 -0400

    cleaned up structure of man function, added analysis function

--- spikevalMain.m
+++ spikevalMain.m
@@ -4,144 +4,29 @@ extracellular multielectrode array spike recording capability
 
 Paper authors:
 Brian D. Allen, Caroline Moore-Kochlacs, Jacob G. Bernstein, 
-Justin P. Kinney, Jorg Scholvin, Luís F. Seoane, Chris Chronopoulos, 
+Justin P. Kinney, Jorg Scholvin, Luis F. Seoane, Chris Chronopoulos, 
 Charlie Lamantia, Suhasa B. Kodandaramaiah, 
 Max Tegmark*, Edward S. Boyden*
 
 Authors of this code:
 Brian D. Allen, Caroline Moore-Kochlacs, Jacob G. Bernstein
 %}
+% Set the root directory for the data
 options.data_dir    = '/media/user/NeuroData1/Dropbox (MIT)/Colocalized Recordings';
-%{
-Pipette localization: the pipette tip sends a beacon signal that
-is detected across the electrode array. The amplitude of this signal 
-across electrodes, combined with the known geometry of the electrodes, 
-allows for an estimate of distance of the pipette tip to the electrode 
-array. A 1/r model of voltage dropoff was shown to be appropriate for this.
+% Run pipette tracking on the exemplar recording from figure 1b-f
+options.run_pipette_tracking_exemplar           = 1;
+% Run pipette tracking on all recordings, which are summarized in figure 1h
+% (may take several minutes)
+options.run_pipette_tracking_all_recordings     = 0;
+% Generate an example ROC curve (like figure 3c, but with 1 minute instead of ~8 minutes of data)
+options.run_roc_example                         = 1;
+% Generate an example voltage estimator (like figure 5, but with 1 minute instead of ~8 minutes of data)
+options.run_voltage_estimator_example           = 1;
+% Generate voltage estimators for all recordings and produce AUCs. This was
+% tested on a computer with 128GB RAM and took several hours to run.
+options.run_voltage_estimator_all_recordings    = 0;
 
-The pipette localization code is adapted from Jacob Bernstein's original
-code.
-%}
-%% Run this section to do pipette tracking
-% Load data
-options.dataset     = 'pipette_track_exemplar';
-% options.dataset = 'pipette_track_all';
-% options.dataset = 'paper_recordings';
-% options.dataset = 'all_recordings';
-
-options.override_pulse_times = false;
-spikevalLocalizePipette(options);
-
-%%
-%{
-An algorithm for assessing potential spike sorting performance as a 
-function of electrode density and quantity. Extracellular signals are 
-considered as convolutions of the intracellular voltage, and deconvolution
-is used to derive the neuron's true spiking state (intracellular voltage) 
-from the electrode voltages.
-%}
-
-options.dataset                             = 'paper_recordings';
-filename_cell                               = spikevalLoadFilenames(options);
-
-options.total_t                             = 45;%456;
-options.num_neurons                         = 12;
-options.num_convolution_filter_pts          = 512;
-options.zero_out_when_patch_doesnt_spike    = 1;
-options.isi_criterion                       = 0.02; % 20ms
-options.recording_with_only_58_electrodes   = filename_cell{5}; % set this to filename_cell{5} if analyzing the 12 neurons in the paper
-options.working_dir                         = '/media/user/NeuroData1/Dropbox (MIT)/spikeval';
-options.todays_fig_dir                      = '180807';
-
-options.recording2_with_only_58_electrodes  = 'asdsdaazgasgf';
-options.debug_mode                          = 0;
-
-options.reverse_mea = 0;
-
-cd(options.working_dir)
-% Create directories for the project, if they don't already exist
-spikevalGenerateDirLayout(options);
-
-options.use_derivative_vec  = [0 1 1 1 1 0 1 1 1 1 1 1 ];
-
-% The following only has to be run once. It takes a long time (~15 minutes)
-% to run
-
-options.get64BestElectrodes = 0;
-
-if options.get64BestElectrodes
-    spikevalGet64BestElectrodes(filename_cell, options)
-end
-
-load([options.working_dir '/config/electrode_order.mat']) % this loads electrode_order_cell into memory
-
-options.parfor      = 1; % you can use a parallel for loop to process the electrodes of a recording if your machine has 128GB of RAM
-
-%% Create a patch voltage estimator and generate ROC curves
-
-options.num_neurons     = 1;
-
-number_of_ROC_analyses  = 4;
-ROC_cell                = cell(options.num_neurons, number_of_ROC_analyses);
-
-for neuron = 1:options.num_neurons
-    
-    ['Processing neuron ' num2str(neuron)]
-    options.use_derivative          = options.use_derivative_vec(neuron);
-    filename                        = filename_cell{neuron};
-    options.electrode_order         = electrode_order_cell{neuron};
-   % options.virtual_reference       = virtual_reference_cell{neuron};
-    
-    patch_voltage_estimator_struct  = spikevalMakePatchVoltageEstimatorStruct(filename, options);
-    
-    options.experiment_type         = 1; % create a ROC curve on the raw electrode voltage
-    ROC_cell{neuron, options.experiment_type}             = spikevalSetUpROCAnalysis(filename, patch_voltage_estimator_struct, options);
-    
-    options.experiment_type         = 2; % create a ROC curve on the raw electrode voltage
-    ROC_cell{neuron, options.experiment_type}             = spikevalSetUpROCAnalysis(filename, patch_voltage_estimator_struct, options);
-    
-    options.experiment_type         = 3; % create a ROC curve on the raw electrode voltage
-    ROC_cell{neuron, options.experiment_type}             = spikevalSetUpROCAnalysis(filename, patch_voltage_estimator_struct, options);
-    
-    options.experiment_type         = 4; % create a ROC curve on the raw electrode voltage
-    ROC_cell{neuron, options.experiment_type}             = spikevalSetUpROCAnalysis(filename, patch_voltage_estimator_struct, options);
-    
-end
-
-%% Calculate area under ROC curves and get p-values
-AUC_cell = cell(number_of_ROC_analyses,1);
-for ii=1:number_of_ROC_analyses
-    options.experiment_type         = ii;
-    AUC_cell{ii} = spikevalCalculatePValuesForAUCs(ROC_cell, options);
-    
-    current_AUC = AUC_cell{ii}.areas;
-    marker_vec = ['+*.xsd^v><ph'];
-    area_fig = figure
-    for jj=1:length(current_AUC(1,:))
-        if options.debug_mode
-            if jj==13
-                plot(current_AUC(:,jj), 'b')
-            elseif jj==14
-                plot(current_AUC(:,jj), 'g')
-            elseif jj==15
-                plot(current_AUC(:,jj), 'm')
-                
-            else
-                plot(current_AUC(:,jj), 'Color', [.5 .5 .5], 'Marker', marker_vec(jj), 'MarkerSize', 14) 
-            end
-        else
-            plot(current_AUC(:,jj), 'Color', [.5 .5 .5])
-        end
-        
-        hold on        
-    end
-    plot(mean(current_AUC, 2), 'k', 'Marker', '.')
-    title(['Experiment type = ' num2str(ii)])
-    ylim(0:1)
-    savefig(area_fig, [options.working_dir '/figures/' options.todays_fig_dir '/AUC exp ' num2str(ii)])
-   
-end
-save([options.working_dir '/results/' options.todays_fig_dir '/AUC'], 'AUC_cell')
+spikevalRunAnalyses(options);
 
 
 
---
+++ spikevalMakeAUCsFromEstimators.m
@@ -0,0 +1,111 @@
+function [] = spikevalMakeAUCsFromEstimators(options)
+
+%{ 
+An algorithm for assessing potential spike sorting performance as a 
+function of electrode density and quantity. Extracellular signals are 
+considered as convolutions of the intracellular voltage, and deconvolution
+is used to derive the neuron's true spiking state (intracellular voltage) 
+from the electrode voltages.
+%}
+
+options.dataset                             = 'paper_recordings';
+    filename_cell                               = spikevalLoadFilenames(options);
+
+    options.total_t                             = 45;%456;
+    options.num_neurons                         = 12;
+    options.num_convolution_filter_pts          = 512;
+    options.zero_out_when_patch_doesnt_spike    = 1;
+    options.isi_criterion                       = 0.02; % 20ms
+    options.recording_with_only_58_electrodes   = filename_cell{5}; % set this to filename_cell{5} if analyzing the 12 neurons in the paper
+    options.working_dir                         = '/media/user/NeuroData1/Dropbox (MIT)/spikeval';
+    options.todays_fig_dir                      = '180807';
+
+    options.recording2_with_only_58_electrodes  = 'asdsdaazgasgf';
+    options.debug_mode                          = 0;
+
+    options.reverse_mea = 0;
+
+    cd(options.working_dir)
+    % Create directories for the project, if they don't already exist
+    spikevalGenerateDirLayout(options);
+
+    options.use_derivative_vec  = [0 1 1 1 1 0 1 1 1 1 1 1 ];
+
+    % The following only has to be run once. It takes a long time (~15 minutes)
+    % to run
+
+    options.get64BestElectrodes = 0;
+
+    if options.get64BestElectrodes
+        spikevalGet64BestElectrodes(filename_cell, options)
+    end
+
+    load([options.working_dir '/config/electrode_order.mat']) % this loads electrode_order_cell into memory
+
+    options.parfor      = 1; % you can use a parallel for loop to process the electrodes of a recording if your machine has 128GB of RAM
+
+    %% Create a patch voltage estimator and generate ROC curves
+
+    options.num_neurons     = 1;
+
+    number_of_ROC_analyses  = 4;
+    ROC_cell                = cell(options.num_neurons, number_of_ROC_analyses);
+
+    for neuron = 1:options.num_neurons
+
+        ['Processing neuron ' num2str(neuron)]
+        options.use_derivative          = options.use_derivative_vec(neuron);
+        filename                        = filename_cell{neuron};
+        options.electrode_order         = electrode_order_cell{neuron};
+       % options.virtual_reference       = virtual_reference_cell{neuron};
+
+        patch_voltage_estimator_struct  = spikevalMakePatchVoltageEstimatorStruct(filename, options);
+
+        options.experiment_type         = 1; % create a ROC curve on the raw electrode voltage
+        ROC_cell{neuron, options.experiment_type}             = spikevalSetUpROCAnalysis(filename, patch_voltage_estimator_struct, options);
+
+        options.experiment_type         = 2; % create a ROC curve on the raw electrode voltage
+        ROC_cell{neuron, options.experiment_type}             = spikevalSetUpROCAnalysis(filename, patch_voltage_estimator_struct, options);
+
+        options.experiment_type         = 3; % create a ROC curve on the raw electrode voltage
+        ROC_cell{neuron, options.experiment_type}             = spikevalSetUpROCAnalysis(filename, patch_voltage_estimator_struct, options);
+
+        options.experiment_type         = 4; % create a ROC curve on the raw electrode voltage
+        ROC_cell{neuron, options.experiment_type}             = spikevalSetUpROCAnalysis(filename, patch_voltage_estimator_struct, options);
+
+    end
+
+    %% Calculate area under ROC curves and get p-values
+    AUC_cell = cell(number_of_ROC_analyses,1);
+    for ii=1:number_of_ROC_analyses
+        options.experiment_type         = ii;
+        AUC_cell{ii} = spikevalCalculatePValuesForAUCs(ROC_cell, options);
+
+        current_AUC = AUC_cell{ii}.areas;
+        marker_vec = ['+*.xsd^v><ph'];
+        area_fig = figure
+        for jj=1:length(current_AUC(1,:))
+            if options.debug_mode
+                if jj==13
+                    plot(current_AUC(:,jj), 'b')
+                elseif jj==14
+                    plot(current_AUC(:,jj), 'g')
+                elseif jj==15
+                    plot(current_AUC(:,jj), 'm')
+
+                else
+                    plot(current_AUC(:,jj), 'Color', [.5 .5 .5], 'Marker', marker_vec(jj), 'MarkerSize', 14) 
+                end
+            else
+                plot(current_AUC(:,jj), 'Color', [.5 .5 .5])
+            end
+
+            hold on        
+        end
+        plot(mean(current_AUC, 2), 'k', 'Marker', '.')
+        title(['Experiment type = ' num2str(ii)])
+        ylim(0:1)
+        savefig(area_fig, [options.working_dir '/figures/' options.todays_fig_dir '/AUC exp ' num2str(ii)])
+
+    end
+    save([options.working_dir '/results/' options.todays_fig_dir '/AUC'], 'AUC_cell')
